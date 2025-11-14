283560c235b0a9575ce2a46e1a797a6caa8ae60e
waqassalman <waqasworkid@gmail.com>
2025-11-13 23:36:10 -0500

    Backend View commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"habusa_backend_view",
+  base:"/habusa_backend_view",
   plugins: [
     react(),
     tailwindcss()
